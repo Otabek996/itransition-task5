2d17e82f0a6f0870ed415185d8ebbf856c6bfbe5
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-04-27 14:42:03 +0500

    doc: configure tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import keepPreset from "keep-react/preset";
+
 module.exports = {
   content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/keep-react/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [keepPreset],
+};
