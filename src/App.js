80794f97183032465669f785db9021266f73ce90
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-04-29 15:31:11 +0500

    feat: add dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Table from "./components/table";
-// import DropdownMenu from "./components/dropdownMenu";
+import DropdownMenu from "./components/dropdownMenu";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
           <div>
             <p>Region: </p>
             <div>
-              {/* <DropdownMenu /> */}
+              <DropdownMenu />
             </div>
           </div>
           <div></div>
--- src/components/dropdownMenu.js
+++ src/components/dropdownMenu.js
@@ -2,28 +2,39 @@ import React from "react";
 
 import { Menu } from "antd";
 
-export default function DropdownMenuCreator() {
+const items = [
+  {
+    key: "sub1",
+    label: "COUNTRY NAME",
+    children: [
+      {
+        key: "1",
+        label: "Option 1",
+      },
+      {
+        key: "2",
+        label: "Option 2",
+      },
+      {
+        key: "3",
+        label: "Option 3",
+      },
+    ],
+  },
+];
+
+const DropdownMenu = () => {
   return (
     <Menu
       style={{
         width: 256,
       }}
-      defaultSelectedKeys={[1]}
+      defaultSelectedKeys={["1"]}
       defaultOpenKeys={["sub1"]}
       mode="inline"
       items={items}
     />
   );
-}
+};
 
-export function getItem(label, key, children) {
-  return [key, children, label];
-}
-
-const items = [
-  getItem("Navigation Three", "firstMenu", [
-    getItem("Option 1", "one"),
-    getItem("Option 2", "two"),
-    getItem("Option 3", "three"),
-  ]),
-];
+export default DropdownMenu;
